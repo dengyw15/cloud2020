f754c1d2b740fcffc2129505073600f918162d11
dengyw <ywdenguestc@gmail.com>
2020-07-12 11:48:33 +0800

    新建payment4004，并注册é进zookeeper注册中心

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
+
+
+# 说明
+
+payment8001/8002 是注册在Eureka注册中心
+
+server7001 关闭了服务自我保护    payment8001 设置心跳发送数据
+
 # maven依赖
 
 ## Eureka Clinent 导入的依赖
---
+++ cloud-provider-payment8004/src/main/java/com/dyw/springcloud/Controller/Payment8004Controller.java
@@ -0,0 +1,26 @@
+package com.dyw.springcloud.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.zookeeper.discovery.ZookeeperDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/payment8004")
+public class Payment8004Controller {
+
+    @Value("${server.port}")
+    String strPort;
+
+    @Autowired
+    ZookeeperDiscoveryClient discoveryClient;
+
+    @GetMapping("/zk/discovery")
+    public String zkDiscovery() {
+        return discoveryClient.description() + "===" + strPort + "===" + UUID.randomUUID();
+    }
+}
