899b2d195d2f4f8dfe7c2dbbce12e180b61efc02
dengyw <ywdenguestc@gmail.com>
2020-07-21 21:35:03 +0800

    添加Hystrix服务端消费降级配置

--- cloud-provider-hystrix-payment8001/src/main/java/com/dyw/springcloud/HystrixPaymentMain8001.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/dyw/springcloud/HystrixPaymentMain8001.java
@@ -2,10 +2,12 @@ package com.dyw.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker  //开启服务熔断机制
 public class HystrixPaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(HystrixPaymentMain8001.class, args);
--- cloud-provider-hystrix-payment8001/src/main/java/com/dyw/springcloud/service/impl/PaymentServiceImpl.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/dyw/springcloud/service/impl/PaymentServiceImpl.java
@@ -1,6 +1,8 @@
 package com.dyw.springcloud.service.impl;
 
 import com.dyw.springcloud.service.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -13,13 +15,23 @@ public class PaymentServiceImpl implements PaymentService {
     }
 
     @Override
+    @HystrixCommand(fallbackMethod = "payment_timoutHandler",
+            commandProperties =
+                    {@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000")
+    })
     public String payment_timeout(Integer id) {
 
+        int age = 10 /0; //运行异常也会走到兜底的timeouthandler方法
+/*        int time = 1;
         try {
-            TimeUnit.SECONDS.sleep(3);
+            TimeUnit.SECONDS.sleep(time);
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }
+        }*/
         return "Timeout Thread: " + Thread.currentThread().getName() + " timeout, id=" + id;
+    }
+
+    public String payment_timoutHandler(Integer id) {
+        return "TimeoutHandler Thread: " + Thread.currentThread().getName() + " 系统繁忙，请稍后再试！  id=" + id;
     }
 }
